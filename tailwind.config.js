fa6b1f87ee2502c216e73b01c70c4d987b769e0a
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-07-24 16:11:34 -0300

    feat: enhance login page layout and styles
    - Updated the login page layout to use a flex-column for better responsiveness on larger screens.
    - Adjusted the background overlay for improved contrast.
    - Modified the login form styles, including label fonts and input borders for better visibility.
    - Changed loading spinner color to match the new design.
    - Added a new SVG image for visual enhancement at the bottom of the login page.
    - Removed unnecessary CSS rules from the login styles.
    - Updated Tailwind configuration to ensure proper screen breakpoints are applied.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,13 @@ module.exports = {
         sans: ['Urbanist', 'sans-serif'],
       },
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1300px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
