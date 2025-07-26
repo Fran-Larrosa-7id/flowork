f40e56247743e7ffefe2f0c289b969e0eee204b6
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-07-26 18:01:52 -0300

    feat: implement splash screen functionality and styles; add SplashService for initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideEnvironmentInitializer, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SplashService } from './services/splash-service.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideEnvironmentInitializer(() => inject(SplashService))
   ]
 };
